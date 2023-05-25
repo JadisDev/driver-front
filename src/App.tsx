dc03687283cca7ff90e9fa886e3b496d28325c53
jadis jale <jadisjale@gmail.com>
2023-05-25 15:10:16 -0300

    feat: uso do redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import { Provider } from "react-redux";
+
+import store from './store';
+import Driver from './pages/Driver';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/driver" element={<Driver />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
