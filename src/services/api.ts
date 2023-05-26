48fee649e3daacf950140374cd3b5a196d2a2f7b
jadis jale <jadisjale@gmail.com>
2023-05-26 11:19:05 -0300

    feat: isolado chamada axios

---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+class Api {
+    private instance: AxiosInstance;
+
+    constructor() {
+        this.instance = axios.create({
+            baseURL: "http://localhost:8000/api/v1",
+            timeout: 10000,
+            responseType: "json",
+            responseEncoding: "utf8",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        });
+    }
+
+    public async get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.instance.get<T>(url, config);
+    }
+
+    public async post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.instance.post<T>(url, data, config);
+    }
+
+    // Métodos adicionais para outras operações HTTP (post, put, delete, etc.)
+}
+
+const api = new Api();
+export default api;
