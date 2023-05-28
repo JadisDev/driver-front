d3716e61bb5744f538fd7810ce27af96a2e17f76
Jadis Jale <jadisjale@hotmail.com>
2023-05-28 02:00:30 -0300

    feat: adicionado footer

---
+++ src/components/layout/layout-footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './layout-footer.css';
+
+const Footer = () => {
+
+    return (
+        <footer className='footer'>
+            Motoristas
+        </footer>
+    )
+
+}
+
+export default Footer;
+
