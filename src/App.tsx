9386b689a8f190dabe9dbeec5779df973a34e074
Jadis Jale <jadisjale@hotmail.com>
2023-05-28 02:00:47 -0300

    feat: protecao das rotas no front

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,44 @@ import ListDrivers from './pages/Driver/list-driver';
 import Login from './pages/Login';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/layout/layout-navbar';
+import Footer from './components/layout/layout-footer';
+import { Container } from 'react-bootstrap';
+import NotAuthorized from './pages/NotAuthorized';
 
 function App() {
+
+  const login = localStorage.getItem('login');
+  const url = window.location.pathname;
+  if (login === '0' || login === "" || login === null && url !== '/') {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+          <Container>
+            <NotAuthorized />
+          </Container>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
+    )
+  }
 
   return (
     <Provider store={store}>
       <div className="App">
         <BrowserRouter>
-          <Routes>
-            <Route>
-              <Route path="/drivers" element={<ListDrivers />} />
-              <Route path="/" element={<Login />} />
-              <Route path="/driver" element={<SaveDriver />} />
-            </Route>
-          </Routes>
+          <NavBar />
+          <Container>
+            <Routes>
+              <Route>
+                <Route path="/drivers" element={<ListDrivers />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/driver" element={<SaveDriver />} />
+              </Route>
+            </Routes>
+          </Container>
+          <Footer />
         </BrowserRouter>
       </div>
     </Provider>
